d48da290c59b41f99033dd8d51afbe3eac48e02e
Varun Tanwar <newvaruntanwar@gmail.com>
2023-12-12 14:10:47 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        rw.setter("Varun");
+        System.out.println(rw.getter());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    private String name;
+
+    public String getter(){
+        return name;
+    }
+
+    public void setter(String name){
+        this.name = name;
+    }
 }
